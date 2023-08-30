e49cb635b0d19e5c2a25a5a63be1987ba3bb6ae5
Geraldine Martinez <geralmartinez000@gmail.com>
2023-08-30 18:28:48 -0300

    clase

--- git.c
+++ git.c
@@ -1,2 +1,27 @@
 #include <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+
+void main () {
+    int num1, num2;
+
+    printf("Ingresa el primer numero: \n");
+    scanf("%d", &num1);
+
+    printf("Ingresa el segundo numero: \n");
+    scanf("%d", &num2);
+
+    if (num1 > num2) {
+        printf("El numero %i es mayor al numero %i \n", num1, num2);
+    }
+    else if (num1 == num2) {
+        printf("Los dos numeros son iguales");
+    } 
+
+    else {
+        printf("El numero %i es mayor al numero %i \n", num2, num1);
+    }
+
+    system("pause");
+}
